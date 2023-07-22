0c31545186ca0dfc84489a162c23fa3db719095e
gclove2000 <1123448674@qq.com>
2023-07-22 18:39:19 +0800

    完成图标库多重绑定

---
+++ 2_图标库/WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MainWindowViewModel
+    {
+
+        public string Title { get; set; }
+
+        public MyIcon Icon { get; set; }
+
+        public List<MyIcon> IconList { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Title = "字符串搜索";
+            Icon = new MyIcon()
+            {
+                Name = "对象搜索",
+                IconCode = "&#xe645;",
+                IconFont = "\ue645"
+            };
+            
+            IconList = new List<MyIcon>()
+            {
+                new MyIcon()
+                {
+                    Name = "对象集合搜索",
+                    IconCode = "&#xe645;",
+                    IconFont = "\ue645"
+                },
+                new MyIcon()
+                {
+                    Name = "闹钟",
+                    IconCode = "&#xe601;",
+                    IconFont = "\ue601"
+                },
+                new MyIcon()
+                {
+                    Name = "日历",
+                    IconCode = "&#xe65b;",
+                    IconFont = "\ue65b"
+                },
+            };
+
+        }
+    }
+
+
+    public class MyIcon
+    {
+        public string Name { get; set; }
+
+        public string IconCode { get; set; }
+
+        public string IconFont { get; set; }
+    }
+}
