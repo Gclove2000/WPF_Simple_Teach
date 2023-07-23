bceb991293b884cc9592dc753535af162f278c8e
gclove2000 <1123448674@qq.com>
2023-07-23 11:08:18 +0800

    完成简单加法计算器

---
+++ 3_简单加法计算器/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Commands;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel viewModel;
+        public MainWindow()
+        {
+            viewModel = new MainWindowViewModel();
+            this.DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+
+
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        public int AddItem1 { get; set; }
+
+        public int AddItem2 { get; set;}
+
+        public int AddSum { get; set;}
+
+        [DoNotNotify]
+        public DelegateCommand CleanBtn { get; set;}
+        [DoNotNotify]
+        public DelegateCommand AddBtn { get; set;}
+
+        public MainWindowViewModel()
+        {
+            AddItem1 = 0;
+            AddItem2 = 0;
+            AddSum = 0;
+            CleanBtn = new DelegateCommand(() =>
+            {
+                AddItem1 = 0;
+                AddItem2 = 0;
+                AddSum = 0;
+            });
+
+            AddBtn = new DelegateCommand(() =>
+            {
+                AddSum = AddItem1 + AddItem2;
+            });
+
+        }
+    }
+}
