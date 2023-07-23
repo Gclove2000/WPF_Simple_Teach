95908ec5a2f5fd4c07faa59c95d1bf5f700250de
gclove2000 <1123448674@qq.com>
2023-07-23 15:31:10 +0800

    简单的对话框

---
+++ 4_简单对话框/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Prism.Commands;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel { get; set; }
+        public MainWindow()
+        {
+            ViewModel = new MainWindowViewModel();
+            this.DataContext = ViewModel;
+            InitializeComponent();
+        }
+    }
+
+
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        [DoNotNotify]
+        public DelegateCommand AddCommand { get; set; }
+        public ObservableCollection<Item> ItemList { get; set; }
+
+        public DelegateCommand <Item> DeleteCommand {get; set;}
+        
+        public string AddContext { get;set; }
+        public MainWindowViewModel()
+        {
+            AddContext = "";
+            ItemList = new ObservableCollection<Item>();
+            AddItem("小明");
+            AddItem("小刘");
+            AddItem("小红");
+
+            AddCommand = new DelegateCommand(() =>
+            {
+                if(AddContext != "")
+                {
+                    AddItem(AddContext);
+                }
+                AddContext = "";
+            });
+
+            DeleteCommand = new DelegateCommand<Item>((item) =>
+            {
+
+                ItemList.Remove(item);
+
+            });
+        }
+
+        private void AddItem(string Content)
+        {
+            ItemList.Add(new Item { Content = Content,Id = ItemList.Count });
+        }
+
+        
+        
+    }
+
+
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+    }
+
+
+}
